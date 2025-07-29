0b19b0169e8b9afa3c7fd3c907bd580d06b55757
Eswar <pvmohaneswar@gmail.com>
2025-07-29 12:06:19 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - Eswar S205</h1>
-      <p>CI/CD cloud DevOps</p>
-      <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
-      
+      <p>CI/CD cloud DevOps</p>      
     </div>
   );
 }
